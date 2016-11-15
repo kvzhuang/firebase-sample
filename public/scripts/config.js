c970e701d7e1866631a96aa1bf7646db990095e0
kevin.zhuang <kevin.zhuang@104.com.tw>
2016-11-15 10:21:36 +0800

    Fix config.

---
+++ public/scripts/config.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD-js0LzrRgUgBKFd3JTQhKjDsAiEUXJkk",
+    authDomain: "sample2-9e4f9.firebaseapp.com",
+    databaseURL: "https://sample2-9e4f9.firebaseio.com",
+    storageBucket: "sample2-9e4f9.appspot.com",
+    messagingSenderId: "397552495773"
+  };
+  firebase.initializeApp(config);
